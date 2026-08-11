eb79dd79bb3eb62d2c987a8d314625135b5f5357
Caio Matheus <kirodnv@gmail.com>
2026-08-11 17:41:29 -0300

    Move dependency registration to Program

--- src/StudyPlannerAgent.McpServer/Program.cs
+++ src/StudyPlannerAgent.McpServer/Program.cs
@@ -1,10 +1,37 @@
-using StudyPlannerAgent.Application;
-using StudyPlannerAgent.Infrastructure;
+using Npgsql;
+using StudyPlannerAgent.Application.Abstractions;
+using StudyPlannerAgent.Application.Progress;
+using StudyPlannerAgent.Application.StudyPlans;
+using StudyPlannerAgent.Infrastructure.Clock;
+using StudyPlannerAgent.Infrastructure.Persistence.InMemory;
+using StudyPlannerAgent.Infrastructure.Persistence.Postgres;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<GetTodayStudyPlanUseCase>();
+builder.Services.AddScoped<GetWeeklyStudyScheduleUseCase>();
+builder.Services.AddScoped<RecordStudyProgressUseCase>();
+builder.Services.AddScoped<GetProgressSummaryUseCase>();
+
+builder.Services.AddSingleton<IClock, SystemClock>();
+
+var connectionString = builder.Configuration.GetConnectionString("Supabase");
+
+if (string.IsNullOrWhiteSpace(connectionString))
+{
+    builder.Services.AddSingleton<InMemoryStudyData>();
+    builder.Services.AddSingleton<IStudyTopicRepository, InMemoryStudyTopicRepository>();
+    builder.Services.AddSingleton<IStudyScheduleRepository, InMemoryStudyScheduleRepository>();
+    builder.Services.AddSingleton<IStudyProgressRepository, InMemoryStudyProgressRepository>();
+}
+else
+{
+    builder.Services.AddSingleton(_ => NpgsqlDataSource.Create(connectionString));
+    builder.Services.AddScoped<IStudyTopicRepository, PostgresStudyTopicRepository>();
+    builder.Services.AddScoped<IStudyScheduleRepository, PostgresStudyScheduleRepository>();
+    builder.Services.AddScoped<IStudyProgressRepository, PostgresStudyProgressRepository>();
+}
+
 builder.Services
     .AddMcpServer()
     .WithHttpTransport(options =>
